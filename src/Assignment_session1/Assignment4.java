e867989e314376fc0dfc03c3b6189b7a038e6f52
Saisheshu <saisheshu@gmail.com>
2016-05-04 12:29:59 +0530

    Java_Assignment4_Session1
    Performing different Operations on 2 Integer variables with different
    Operators

---
+++ src/Assignment_session1/Assignment4.java
@@ -0,0 +1,19 @@
+package Assignment_session1;
+
+public class Assignment4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int x=5,y=10;
+		
+		System.out.println("X+y*2= "+(x+y*2));
+		System.out.println("X-y+2= "+(x-y+2));
+		System.out.println("(X+y)*2= "+((x+y)*2));
+		System.out.println("y%x= "+(y%x));
+		
+		
+	}
+
+}
